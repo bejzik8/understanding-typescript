807745f8b27282da6c0fdc0a755a1eefd870787e
Basic Mirko <bejzik8@gmail.com>
2023-10-17 01:28:13 +0200

    Advantage of TS over JS.

--- app.ts
+++ app.ts
@@ -1,4 +1,8 @@
 function add(number1: number, number2: number) {
+  // if (typeof number1 !== "number" || typeof number2 !== "number") {
+  //   throw new Error("Incorrect input!");
+  // }
+
   return number1 + number2;
 }
 
