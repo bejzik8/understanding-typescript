35d91e0d935568d4dc28014a747103b0a47432c6
Basic Mirko <bejzik8@gmail.com>
2023-10-17 22:49:51 +0200

    Union types...

--- app.ts
+++ app.ts
@@ -1,48 +1,18 @@
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
+function combine(input1: number | string, input2: number | string) {
+  let result;
 
-// const person: {
-//   name: string;
-//   age: number;
-//   hobbies: string[];
-//   role: [number, string]; // tuple
-// } = {
-//   name: 'Mirko',
-//   age: 30,
-//   hobbies: ['Sports', 'Cooking'],
-//   role: [2, 'author']
-// }
+  if (typeof input1 === 'number' && typeof input2 === 'number') {  
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
 
-// const ADMIN = 0;
-// const READ_ONLY = 1;
-// const AUTHOR = 2;
-
-enum Role { ADMIN = 'ADMIN', READ_ONLY = 100, AUTHOR = 200 };
-
-const person = {
-  name: 'Mirko',
-  age: 30,
-  hobbies: ['Sports', 'Cooking'],
-  role: Role.ADMIN
+  return result;
 }
 
-// person.role.push('admin');
-// person.role[1] = 10;
+const combinedAges = combine(30, 26);
 
-// person.role = [0, 'admin', 'user'];
+console.log(combinedAges);
 
-let favoriteActivities: string[];
-
-favoriteActivities = ['Sports'];
-
-console.log(person.name)
-
-for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase());
-}
-
-if (person.role === Role.AUTHOR) {
-  console.log('is author');
-}
+const combinedNames = combine('Mirko', 'Anna');
+console.log(combinedNames);
