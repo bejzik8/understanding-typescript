c8f60cd92bb4cb49943ef641a005912e3546e24a
Basic Mirko <bejzik8@gmail.com>
2023-10-18 00:16:04 +0200

    Function types and callbacks...

--- app.ts
+++ app.ts
@@ -6,6 +6,12 @@ function printResult(num: number): void {
   console.log('Result: ' + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+
+  cb(result);
+}
+
 printResult(add(5, 12));
 
 let combineValues: (a: number, b: number) => number;
@@ -17,3 +23,9 @@ combineValues = add;
 console.log(combineValues(8, 8));
 
 // let someValue: undefined;
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+  
+  // return result;
+});
