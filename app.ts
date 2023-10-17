d67b9a561bbe5432e908f70bd85122e5d855b3ee
Basic Mirko <bejzik8@gmail.com>
2023-10-18 00:24:04 +0200

    The "never" type...

--- app.ts
+++ app.ts
@@ -7,3 +7,13 @@ userInput = 'Max';
 if (typeof userInput === 'string') {
   userName = userInput;
 }
+
+function generateError(message: string, code: number): never {
+  throw { message, errorCode: code };
+  // while (true) {}
+}
+
+generateError('An error occurred!', 500);
+
+// const result = generateError('An error occurred!', 500);
+// console.log(result);
