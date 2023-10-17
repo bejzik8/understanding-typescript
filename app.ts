d5b3213de2bab796487e634bdc23aec8ef63dd20
Basic Mirko <bejzik8@gmail.com>
2023-10-17 22:27:06 +0200

    Array types...

--- app.ts
+++ app.ts
@@ -5,7 +5,16 @@
 
 const person = {
   name: 'Mirko',
-  age: 30
+  age: 30,
+  hobbies: ['Sports', 'Cooking']
 }
 
-console.log(person)
+let favoriteActivities: string[];
+
+favoriteActivities = ['Sports'];
+
+console.log(person.name)
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
