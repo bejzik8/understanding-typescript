bc503fbee518eada2ca9b3dd953f30aa54f6149f
Basic Mirko <bejzik8@gmail.com>
2023-10-18 23:59:02 +0200

    Arrow functions...

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,11 @@ let age = 30;
 
 age = 29;
 
-function add(a: number, b: number) {
-  let result;
-  result = a + b;
-  return result;
-}
+// function add(a: number, b: number) {
+//   let result;
+//   result = a + b;
+//   return result;
+// }
 
 // WORKS IN JS
 // if (age > 20) {
@@ -17,3 +17,15 @@ function add(a: number, b: number) {
 // }
 
 // console.log(isOld);
+
+const sum = (a: number, b: number = 1) => a + b;
+
+const printOutput: (a: number | string) => void = output => console.log(output);
+
+printOutput(sum(2, 5));
+
+const btn = document.querySelector('button');
+
+if (btn) {
+  btn.addEventListener('click', event => console.log(event));
+}
