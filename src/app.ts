bffbc615c7a921f98bf3d750411dc5521fd25640
Basic Mirko <bejzik8@gmail.com>
2023-10-18 11:08:15 +0200

    Covered strict compile options.

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-const user = 'Max';
+const button = document.querySelector('button')!;
+
+function clickHandler(message: string) {
+  console.log('Clicked! ' + message);
+}
+
+// a comment
+if (button) {
+  button.addEventListener('click', clickHandler.bind(null, 'You\'re welcome!'));
+}
