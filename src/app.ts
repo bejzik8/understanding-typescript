c5cf6b5ec2961be295603e4316db81eef9886913
Basic Mirko <bejzik8@gmail.com>
2023-10-20 12:35:55 +0200

    Discriminated unions...

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,7 @@ type Numeric = number | boolean;
 
 type Universal = CombinableT & Numeric; // number
 
-function add(a: number, b: number) {
+function adds(a: number, b: number) {
   // type guard
   if (typeof a === 'string' || typeof b === 'string') {
     return a.toString() + b.toString()
@@ -79,3 +79,33 @@ function useVehicle(vehicle: Vehicle) {
 
 useVehicle(v1);
 useVehicle(v2);
+
+interface Bird {
+  type: 'bird';
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: 'horse';
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  let speed;
+
+  switch (animal.type) {
+    case 'bird':
+      speed = animal.flyingSpeed;
+      break;
+
+    case 'horse':
+      speed = animal.runningSpeed;
+      break;
+  }
+
+  console.log('Moving at speed: ' + speed);
+}
+
+moveAnimal({ type: 'bird', flyingSpeed: 10 });
