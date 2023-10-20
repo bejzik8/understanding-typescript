e5f421e63173b3bd9eee8deb71ea201f90b6a093
Basic Mirko <bejzik8@gmail.com>
2023-10-20 16:44:17 +0200

    The "keyof" constraint...

--- src/app.ts
+++ src/app.ts
@@ -37,3 +37,12 @@ function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
 }
 
 console.log(countAndDescribe('Hi there!'));
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return obj[key];
+}
+
+extractAndConvert({ name: 'Mirko' }, 'name');
