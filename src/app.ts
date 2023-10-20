9b5a5952ff4d364dd89b068e27c765a0fd696910
Basic Mirko <bejzik8@gmail.com>
2023-10-20 02:09:53 +0200

    Abstract classes...

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
-class Department {
+abstract class Department {
   static fiscalYear = 2020;
   // private readonly id: string;
   // private name: string;
   protected employees: string[] = [];
 
-  constructor(private readonly id: string, public name: string) {
+  constructor(protected readonly id: string, public name: string) {
     // this.id = id;
     // this.name = n;
     console.log(Department.fiscalYear);
@@ -14,9 +14,7 @@ class Department {
     return { name }
   }
 
-  describe(this: Department) {
-    console.log(`Department (${this.id}): ${this.name}`);
-  }
+  abstract describe(this: Department): void;
 
   addEmployee(employee: string) {
     // this.id = 'd2';
@@ -35,6 +33,10 @@ class ITDepartment extends Department {
   constructor(id: string, admins: string[]) {
     super(id, 'IT');
     this.admins = admins;
+  }
+
+  describe(): void {
+    console.log('IT Department - ID: ' + this.id);
   }
 }
 
@@ -60,6 +62,10 @@ class AccountingDepartment extends Department {
   constructor(id: string, private reports: string[]) {
     super(id, 'Accounting');
     this.lastReport = reports[0];
+  }
+
+  describe(): void {
+    console.log('Accounting Department - ID: ' + this.id);
   }
 
   addEmployee(employee: string): void {
