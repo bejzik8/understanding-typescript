28ffb7200c0c1b2fa25703fbe385bfa5c94cbe70
Basic Mirko <bejzik8@gmail.com>
2023-10-20 02:25:19 +0200

    Using intefaces with classes...

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,32 @@
-interface Person {
+interface Greetable {
   name: string;
-  age: number;
 
   greet(phrase: string): void;
 }
 
-let user1: Person;
+class Person implements Greetable {
+  name: string;
+  age = 30;
 
-user1 = {
-  name: 'Mirko',
-  age: 30,
-  greet(phrase: string) {
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  greet(phrase: string): void {
     console.log(phrase + ' ' + this.name);
   }
 }
+
+let user1: Greetable;
+
+// user1 = {
+//   name: 'Mirko',
+//   age: 30,
+//   greet(phrase: string) {
+//     console.log(phrase + ' ' + this.name);
+//   }
+// }
+
+user1 = new Person('Mirko');
 
 user1.greet('Hi there - I am');
