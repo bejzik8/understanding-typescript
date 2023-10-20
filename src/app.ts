d53cdbe03a8985ce4969bdbc65bdaf3c747a7a19
Basic Mirko <bejzik8@gmail.com>
2023-10-20 14:42:50 +0200

    Built-in Generics...

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-// Code here!
+// const names: Array<string> = [];
+
+// // names[0].split(' ');
+
+// const promise: Promise<number> = new Promise((resolve, _) => {
+//   setTimeout(() => {
+//     resolve(10);
+//   }, 2000);
+// });
+
+// promise.then(data => {
+//   // data.split(' ');
+// });
