3501266475c00ccbebe2e420c8cd70f24f749a6b
Basic Mirko <bejzik8@gmail.com>
2023-10-21 01:28:39 +0200

    Example: autobind decorator.

--- src/app.ts
+++ src/app.ts
@@ -101,3 +101,34 @@ class Product {
 
 const p1 = new Product('Book', 19);
 const p2 = new Product('Book 2', 29);
+
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+
+      return boundFn;
+    }
+  }
+
+  return adjDescriptor;
+}
+
+class Printer {
+  message = 'This works!';
+
+  @Autobind
+  showMessage() {
+    console.log(this.message);
+  }
+}
+
+const p = new Printer();
+
+const btn = document.querySelector('button')!;
+// btn.addEventListener('click', p.showMessage.bind(p)); // w/o autobind decorator
+btn.addEventListener('click', p.showMessage.bind(p));
