f630da986b600c1a5842c2c354346ac5f5d0128a
Basic Mirko <bejzik8@gmail.com>
2023-10-20 14:52:06 +0200

    Creating a Generic function...

--- src/app.ts
+++ src/app.ts
@@ -11,3 +11,11 @@
 // promise.then(data => {
 //   // data.split(' ');
 // });
+
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
+
+console.log(mergedObj.name);
