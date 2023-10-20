002f4921be9a51b795c4fa0cbe870676a8a057a4
Basic Mirko <bejzik8@gmail.com>
2023-10-20 12:43:26 +0200

    Type casting...

--- src/app.ts
+++ src/app.ts
@@ -109,3 +109,16 @@ function moveAnimal(animal: Animal) {
 }
 
 moveAnimal({ type: 'bird', flyingSpeed: 10 });
+
+// Type Casting
+
+// 1.
+// const userInputElement = <HTMLInputElement>document.getElementById('user-input');
+// 2.
+// const userInputElement = document.getElementById('user-input') as HTMLInputElement;
+
+const userInputElement = document.getElementById('user-input');
+
+if (userInputElement) {
+  (userInputElement as HTMLInputElement).value = 'Hi there!';
+}
